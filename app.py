dbbf6c3a74e775e71a15c43d05b176f40be1761c
aizathizami <97604188+aizathizami@users.noreply.github.com>
2022-04-02 16:38:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,35 +32,67 @@ def n_neighbors_uri_audio(genre, start_year, end_year, test_feat):
     audios = genre_data.iloc[n_neighbors][audio_feats].to_numpy()
     return uris, audios
 
+def page():
+    title = "Song Recommendation Engine"
+    st.title(title)
 
-title = "Song Recommendation Engine"
-st.title(title)
+    st.write("First of all, welcome! This is the place where you can customize what you want to listen to based on genre and several key audio features. Try playing around with different settings and listen to the songs recommended by our system!")
+    st.markdown("##")
 
-st.write("First of all, welcome! This is the place where you can customize what you want to listen to based on genre and several key audio features. Try playing around with different settings and listen to the songs recommended by our system!")
-st.markdown("##")
+    with st.container():
+        col1, col2,col3,col4 = st.columns((2,0.5,0.5,0.5))
+        with col3:
+            st.markdown("***Choose your genre:***")
+            genre = st.radio(
+                "",
+                genre_names, index=genre_names.index("Pop"))
+        with col1:
+            st.markdown("***Choose features to customize:***")
+            start_year, end_year = st.slider(
+                'Select the year range',
+                1990, 2019, (2015, 2019)
+            )
+            acousticness = st.slider(
+                'Acousticness',
+                0.0, 1.0, 0.5)
+            danceability = st.slider(
+                'Danceability',
+                0.0, 1.0, 0.5)
+            energy = st.slider(
+                'Energy',
+                0.0, 1.0, 0.5)
+            instrumentalness = st.slider(
+                'Instrumentalness',
+                0.0, 1.0, 0.0)
+            valence = st.slider(
+                'Valence',
+                0.0, 1.0, 0.45)
+            tempo = st.slider(
+                'Tempo',
+                0.0, 244.0, 118.0)
 
-tracks_per_page = 6
-test_feat = [acousticness, danceability, energy, instrumentalness, valence, tempo]
-uris, audios = n_neighbors_uri_audio(genre, start_year, end_year, test_feat)
+    tracks_per_page = 6
+    test_feat = [acousticness, danceability, energy, instrumentalness, valence, tempo]
+    uris, audios = n_neighbors_uri_audio(genre, start_year, end_year, test_feat)
 
-tracks = []
-for uri in uris:
-    track = """<iframe src="https://open.spotify.com/embed/track/{}" width="260" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>""".format(uri)
-    tracks.append(track)
+    tracks = []
+    for uri in uris:
+        track = """<iframe src="https://open.spotify.com/embed/track/{}" width="260" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>""".format(uri)
+        tracks.append(track)
 
-if 'previous_inputs' not in st.session_state:
-    st.session_state['previous_inputs'] = [genre, start_year, end_year] + test_feat
+    if 'previous_inputs' not in st.session_state:
+        st.session_state['previous_inputs'] = [genre, start_year, end_year] + test_feat
     
-current_inputs = [genre, start_year, end_year] + test_feat
-if current_inputs != st.session_state['previous_inputs']:
-    if 'start_track_i' in st.session_state:
+    current_inputs = [genre, start_year, end_year] + test_feat
+    if current_inputs != st.session_state['previous_inputs']:
+        if 'start_track_i' in st.session_state:
+            st.session_state['start_track_i'] = 0
+        st.session_state['previous_inputs'] = current_inputs
+
+    if 'start_track_i' not in st.session_state:
         st.session_state['start_track_i'] = 0
-    st.session_state['previous_inputs'] = current_inputs
-
-if 'start_track_i' not in st.session_state:
-    st.session_state['start_track_i'] = 0
     
-with st.container():
+    with st.container():
         col1, col2, col3 = st.columns([2,1,2])
         if st.button("Recommend More Songs"):
             if st.session_state['start_track_i'] < len(tracks):
@@ -100,40 +132,5 @@ with st.container():
 
         else:
             st.write("No songs left to recommend")
-    
-    
-with st.sidebar:
-    with st.container():
-        col1, col2,col3,col4 = st.columns((2,0.5,0.5,0.5))
-        with col3:
-            st.markdown("***Choose your genre:***")
-            genre = st.radio(
-                "",
-                genre_names, index=genre_names.index("Pop"))
-        with col1:
-            st.markdown("***Choose features to customize:***")
-            start_year, end_year = st.slider(
-                'Select the year range',
-                1990, 2019, (2015, 2019)
-            )
-            acousticness = st.slider(
-                'Acousticness',
-                0.0, 1.0, 0.5)
-            danceability = st.slider(
-                'Danceability',
-                0.0, 1.0, 0.5)
-            energy = st.slider(
-                'Energy',
-                0.0, 1.0, 0.5)
-            instrumentalness = st.slider(
-                'Instrumentalness',
-                0.0, 1.0, 0.0)
-            valence = st.slider(
-                'Valence',
-                0.0, 1.0, 0.45)
-            tempo = st.slider(
-                'Tempo',
-                0.0, 244.0, 118.0)
 
-    
-
+page()
